261acf9dd8f56e2d57998dc136bc719fafa87324
TeodoraDumitru99 <teodum.1999@gmail.com>
2025-02-24 17:44:35 +0200

    hero-hero prototype, route to game shortcut

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import TicTacToe from "./components/tic_tac_toe/TicTacToe";
 import Memory from "./components/memory/Memory";
+import Hero from "./components/hero/Hero";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <main>
-        <TicTacToe />
-        <Memory />
-      </main>
-    </>
+    <Router>
+      <>
+        <main>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/tictactoe" element={<TicTacToe />} />
+            <Route path="/cardmemory" element={<Memory />} />
+          </Routes>
+        </main>
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,64 @@
+import "./hero.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import TicImg from "../assets/tictactoe.png";
+import MemoryImg from "../assets/memory.png";
+
+const Hero = () => {
+  const [modalOpen, setModalOpen] = useState(true);
+
+  const gameLinks = [
+    { key: 0, href: "/tictactoe", text: "Tic Tac Toe", imgsrc: TicImg },
+    { key: 1, href: "/cardmemory", text: "Memory Card", imgsrc: MemoryImg },
+  ];
+
+  return (
+    <section className="hero">
+      <div className="wrapper">
+        <div className="hero_content">
+          {modalOpen ? (
+            <div className="hero_modal">
+              <h1 className="hero_header">MINI ARCADE</h1>
+              <h2 className="hero_sutitle">
+                Revisit your favourite classic games
+              </h2>
+              <button
+                className="hero_game_button"
+                onClick={() => setModalOpen(false)}
+              >
+                Games
+              </button>
+            </div>
+          ) : (
+            <div className="hero_games">
+              <span
+                className="hero_back_button"
+                onClick={() => setModalOpen(true)}
+              >
+                ◄ Back
+              </span>
+              <ul className="hero_list">
+                {gameLinks.map((gameLink) => (
+                  <div key={gameLink.key}>
+                    <Link to={gameLink.href}>
+                      <li className="hero_game_group">
+                        <img
+                          src={gameLink.imgsrc}
+                          alt={gameLink.text}
+                          className="hero_game_img"
+                        />
+                        <span>{gameLink.text}</span>
+                      </li>
+                    </Link>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
